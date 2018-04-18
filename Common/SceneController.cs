3b24e5cc759d44cb8e44a596ba7bf0fc7f50db5f
murakami <murakamitakahiro@murakami-no-MacBook-Pro.local>
2018-04-19 01:06:53 +0900

    Third commit(so tired)

---
+++ Common/SceneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneController : SingletonMonoBehaviourFast<SceneController> {
+
+	public string StartScene;
+
+	void Start(){
+		LoadCityScene();
+	}
+
+	public void LoadCityScene(){
+		StartCoroutine (LoadSceneAndSetActive (StartScene));
+	}
+
+	private IEnumerator LoadSceneAndSetActive (string sceneName)
+	{
+			yield return SceneManager.LoadSceneAsync (sceneName, LoadSceneMode.Additive);
+			Scene newlyLoadedScene = SceneManager.GetSceneAt (SceneManager.sceneCount - 1);
+
+			SceneManager.SetActiveScene (newlyLoadedScene);
+	}
+}
---
+++ Menu/MenuUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MenuUI : MonoBehaviour {
+
+	public Button NewGame;
+	public Button Option;
+	public Button Exit;
+
+	// Use this for initialization
+	void Start () {
+		SetButtons();
+	}
+
+	private void SetButtons(){
+		NewGame.onClick.AddListener(delegate{Debug.Log("new game");});
+		NewGame.onClick.AddListener(delegate{LoadCityScene();});
+		Option.onClick.AddListener(delegate{ShowOptionUI();});
+		Exit.onClick.AddListener(delegate{Application.Quit();});
+
+	}
+
+
+
+	public void LoadCityScene(){
+		StartCoroutine (LoadSceneAndSetActive ("Base"));
+	}
+
+	private IEnumerator LoadSceneAndSetActive (string sceneName)
+	{
+			yield return SceneManager.LoadSceneAsync (sceneName, LoadSceneMode.Additive);
+			Scene newlyLoadedScene = SceneManager.GetSceneAt (SceneManager.sceneCount - 1);
+
+			SceneManager.SetActiveScene (newlyLoadedScene);
+	}
+
+
+	public void ShowOptionUI(){
+		Debug.Log("OptionUI");
+	}
+
+
+}
